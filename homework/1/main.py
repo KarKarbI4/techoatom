ac634bb535faf3bce908f766f6e3d5c6dbe679f7
KarKarbI4 <tvs.private@gmail.com>
2016-09-29 20:39:52 +0300

    Added Charge type conversion

--- homework/1/main.py
+++ homework/1/main.py
@@ -1,6 +1,6 @@
 class Charge:
     def __init__(self, value):
-        self._value = value
+        self._value = float(value)
 
     value = property()
     @value.getter
@@ -8,5 +8,6 @@ class Charge:
         return round(self._value, 2)
 
 if __name__ == '__main__':
+    charge = Charge(3)
     charge = Charge(3.232324323)
     print(charge.value)
