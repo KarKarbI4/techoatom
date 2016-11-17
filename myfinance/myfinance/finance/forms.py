4a30e690a2cdd00c45b11af928be0b10d2f02221
Victor Tyunyakov <tvs.private@gmail.com>
2016-11-17 18:21:29 +0300

    Added Charge model and forms

--- myfinance/myfinance/finance/forms.py
+++ myfinance/myfinance/finance/forms.py
@@ -2,15 +2,21 @@ from datetime import datetime
 from datetime import date
 
 from django import forms
+from django.forms import ModelForm
+from finance.models import Charge
 from django.core.exceptions import ValidationError
 
-class ChargeForm(forms.Form):
-    value = forms.DecimalField(label='Value', required=True)
-    date = forms.DateField(label='Date', required=True)
-    
+
+class ChargeForm(ModelForm):
+
+    class Meta:
+        model = Charge
+        fields = ['value', 'date']
+
     def clean(self):
         cleaned_data = super().clean()
         if cleaned_data.get('value') < 0 and \
-        cleaned_data.get('date') > date.today():
-            self.add_error('date', 'Charges off are not supported for future days. Please, try again.')
+                cleaned_data.get('date') > date.today():
+            self.add_error(
+                'date', 'Charges off are not supported for future days. Please, try again.')
         return cleaned_data
--- myfinance/myfinance/finance/models.py
+++ myfinance/myfinance/finance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Charge(models.Model):
+
+    value = models.DecimalField(decimal_places=2, max_digits=300)
+    date = models.DateField()
+
+    class Meta:
+        db_table = 'charges'
